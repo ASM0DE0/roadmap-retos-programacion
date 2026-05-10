4129fbe85d1df6703bdd82188f44b55e68369b65
ASM0DE0 <eduardo.basoalto.g@gmail.com>
2026-05-10 10:19:39 -0400

    Create ASM0DE0.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/ASM0DE0.py
@@ -0,0 +1,10 @@
+# https://www.python.org/
+
+""" esto es un comentario """
+
+''' esto tambien es un comentario '''
+
+my_variable = "comentario a"
+my_variable = "comentario b"
+
+my_contstant = "la constante" # esto es para que no se cambie 
