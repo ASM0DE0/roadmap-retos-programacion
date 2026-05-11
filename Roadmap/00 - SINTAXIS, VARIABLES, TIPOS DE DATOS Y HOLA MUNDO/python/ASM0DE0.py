ac199e839b04d5bdd54a0b9cc00722adaa357f28
ASM0DE0 <eduardo.basoalto.g@gmail.com>
2026-05-11 16:28:33 -0400

    Create ASM0DE0.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/ASM0DE0.py
@@ -0,0 +1,36 @@
+#https://.python.org/
+
+#esto es un comentario
+
+"esto tambien es un comentario"
+
+"""esto es otro comentario
+que se puede hacer asi"""
+
+'''
+esto es otro tipo de comentario
+que tambien se puede hacer asi
+'''
+
+my_variable = "comentario a"
+my_variable = "comentario b"
+
+MY_CONSTANT = "esto no cambia" #no existen constantes en python
+
+# DATOS PRIMITIVOS
+
+#int es para numeros enteros
+my_int = 1
+
+#float es para numeros decimales
+my_float = 1.6
+
+#bool es para binario osea verdadero o falso
+my_bool = True
+my_bool = False
+
+#string es cadena de texto
+my_string = "esto es un cadena de texto"
+my_string_a = "esta es la segunda cadena de texto"
+
+print ("hola, python")
